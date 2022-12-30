9991da29974e7456d9eaa159c4acea5137194f52
JayavelAnandh <jvanandh984@gmail.com>
2022-12-30 19:34:34 +0530

    Added DOM-Elements

--- script.js
+++ script.js
@@ -19,29 +19,76 @@ fetch("https://restcountries.com/v3.1/all")
 .catch((error)=>console.log(error))
 
 let Whole = document.createElement("div")
-Whole.setAttribute("class","container-lg")
+Whole.setAttribute("class","container")
 document.body.append(Whole)
+let Heading = document.createElement("H1")
+Heading.innerHTML ="WORLS STATISTICS"
+Heading.setAttribute("class","text-center")
+Heading.setAttribute("id","title")
+Whole.append(Heading)
 let row = document.createElement("div")
 row.setAttribute("class","row")
 Whole.append(row)
 function Createdetails({name,flag,latlng,region,capital,lat,long,code}){
-    row.innerHTML +=
     
-    `
-    <div class= "col-lg-4 col-sm-12 col-md-6">
-    <div class="card  text-light" style="width: 18rem;" id="Card">
-    <H4 class =" text-center card-header" id="title" ">${name}</H4>
-    <img src="${flag}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <p><span>Country code:</span>${code}</p>
-      <p><span>latlng : </span>${latlng}</p>
-      <p><span>Region : </span>${region}</p>
-      <p><span>Capital : </span>${capital}</p>
-      <button class="btn btn-primary"  onclick="Weather(${lat},${long})"  "id ="buttons">Click For Weather</button>
-    </div>
-  </div>
-    </div>
-    `
+
+  var columnchange = document.createElement("div")
+  columnchange.setAttribute("class","col-lg-4 col-sm-12 col-md-6");
+  row.append(columnchange)
+
+  var Carddiv = document.createElement("div")
+  Carddiv.setAttribute("class","card  text-light")
+  Carddiv.setAttribute("id","Card")
+  columnchange.append(Carddiv)
+
+  var Cardhead = document.createElement("H4")
+  Cardhead.setAttribute("class","text-center card-header")
+  Cardhead.innerHTML += name
+  Carddiv.append(Cardhead)
+
+  var imageFlag = document.createElement("img")
+  imageFlag.setAttribute("src",flag)
+  imageFlag.setAttribute("class","card-img-top")
+  Carddiv.append(imageFlag)
+
+  var Cardbody= document.createElement("div")
+  Cardbody.setAttribute("class","card-body")
+  Carddiv.append(Cardbody)
+
+  var P1 = document.createElement("p")
+  Cardbody.append(P1)
+
+  var P2 = document.createElement("p")
+  Cardbody.append(P2)
+  
+  var P3 = document.createElement("p")
+  Cardbody.append(P3)
+
+  var P4 = document.createElement("p")
+  Cardbody.append(P4)
+
+  var S1 = document.createElement("span")
+  S1.innerHTML=`Country code :${code}`
+  P1.append(S1)
+
+  var S2 = document.createElement("span")
+  S2.innerHTML=`latlng${latlng}`
+  P2.append(S2)
+
+  var S3 = document.createElement("span")
+  S3.innerHTML=`Region :${region}`
+  P3.append(S3)
+
+  var S4 = document.createElement("span")
+  S4.innerHTML=`Capital :${capital}`
+  P4.append(S4)
+
+  var Weatherbutton = document.createElement("button")
+  Weatherbutton.setAttribute("class","btn btn-primary")
+  Weatherbutton.addEventListener("click",()=>{Weather(lat,long)})
+  Weatherbutton.setAttribute("id","buttons")
+  Weatherbutton.innerHTML="Click For Weather"
+  Cardbody.append(Weatherbutton)
 }
 
 async function Weather(lat,long){
